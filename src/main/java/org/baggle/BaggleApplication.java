b11291c04a576b727a8224ee193224c3367f1b03
sunwoong <sunwoong1122@gmail.com>
2023-07-25 19:39:40 +0900

    [init] #1 프로젝트 세팅

---
+++ src/main/java/org/baggle/BaggleApplication.java
@@ -0,0 +1,11 @@
+package org.baggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaggleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaggleApplication.class, args);
+    }
+}
---
+++ src/test/java/org/baggle/BaggleApplicationTests.java
@@ -0,0 +1,11 @@
+package org.baggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaggleApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
