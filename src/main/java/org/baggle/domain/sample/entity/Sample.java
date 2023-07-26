8be2db6ab08427dbcf2cf0f7c2c52675429c5a92
sunwoong <sunwoong1122@gmail.com>
2023-07-26 17:06:00 +0900

    [feat] #1 sample entity 구현

---
+++ src/main/java/org/baggle/domain/sample/entity/Sample.java
@@ -0,0 +1,23 @@
+package org.baggle.domain.sample.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Sample {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sample_id")
+    private Long id;
+    private String data;
+
+    @Builder
+    public Sample(String data) {
+        this.data = data;
+    }
+}
