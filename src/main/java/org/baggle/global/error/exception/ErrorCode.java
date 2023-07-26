040bc6142e0212221889fc40354be2673ca8d041
sunwoong <sunwoong1122@gmail.com>
2023-07-26 17:03:58 +0900

    [feat] #1 Base Error Code 구현

---
+++ src/main/java/org/baggle/global/error/exception/ErrorCode.java
@@ -0,0 +1,48 @@
+package org.baggle.global.error.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorCode {
+    /**
+     * 400 Bad Request
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+    /**
+     * 401 Unauthorized
+     */
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
+
+    /**
+     * 403 Forbidden
+     */
+    FORBIDDEN(HttpStatus.FORBIDDEN, "리소스 접근 권한이 없습니다."),
+
+    /**
+     * 404 Not Found
+     */
+    ENTITY_NOT_FOUND(HttpStatus.BAD_REQUEST, "엔티티를 찾을 수 없습니다."),
+
+    /**
+     * 405 METHOD NOT ALLOWED
+     */
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "잘못된 HTTP method 요청입니다."),
+
+    /**
+     * 409 Conflict
+     */
+    CONFLICT(HttpStatus.CONFLICT, "이미 존재하는 리소스입니다."),
+
+    /**
+     * 500 Internal Server Error
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
