170871e95ca7735bbe2de59b77067e64ea4375f9
sunwoong <sunwoong1122@gmail.com>
2023-07-26 17:07:14 +0900

    [feat] #1 sample request dto 구현

---
+++ src/main/java/org/baggle/domain/sample/dto/request/SampleRequestDto.java
@@ -0,0 +1,24 @@
+package org.baggle.domain.sample.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.baggle.domain.sample.entity.Sample;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class SampleRequestDto {
+    private String data;
+
+    @Builder
+    public SampleRequestDto(String data) {
+        this.data = data;
+    }
+
+    public Sample toEntity() {
+        return Sample.builder()
+                .data(data)
+                .build();
+    }
+}
