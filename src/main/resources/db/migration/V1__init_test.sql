75e65c79c55777b84343b28e71377b92bba3273c
RyuKwanKon <rhks33rhs@gmail.com>
2023-07-28 19:10:04 +0900

    [feat] #4 flyway로 sample table 생성

---
+++ src/main/resources/db/migration/V1__init_test.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS sample;
+CREATE TABLE sample(
+                                     `sample_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                     `data` VARCHAR(255),
+                                     PRIMARY KEY (`sample_id`));
