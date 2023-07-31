1b710e0c834eab8e314ca8015dad6a284e5627f1
sunwoong <sunwoong1122@gmail.com>
2023-08-01 00:58:26 +0900

    [feat] #8 jwt authentication filter 구현

---
+++ src/main/java/org/baggle/global/config/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package org.baggle.global.config.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.baggle.global.config.jwt.JwtProvider;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer ";
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String accessToken = getAccessTokenFromHttpServletRequest(request);
+        jwtProvider.validateAccessToken(accessToken);
+        Long userId = jwtProvider.getSubject(accessToken);
+        UserAuthentication authentication = new UserAuthentication(userId, null, null);
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String getAccessTokenFromHttpServletRequest(HttpServletRequest request) {
+        String accessToken = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(accessToken) && accessToken.startsWith(BEARER)) {
+            return accessToken.substring(BEARER.length());
+        }
+        return null;
+    }
+}
