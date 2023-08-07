c4e9e885f2e74711d1a789ff1dbf8794620fa034
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-08 02:13:51 +0900

    [feat] #16 redis expire 를 이용한 긴급알람 구현

---
+++ src/main/java/org/baggle/infra/redis/ExpirationListener.java
@@ -0,0 +1,32 @@
+package org.baggle.infra.redis;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.baggle.domain.fcm.domain.FcmToken;
+import org.baggle.domain.fcm.service.FcmNotificationService;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ExpirationListener implements MessageListener {
+    private final FcmNotificationService fcmNotificationService;
+
+    /**
+     * cache가 만료되었을 때 실행되는 메서드입니다.
+     * 긴급알람과 관련된 cache를 소모할 때 알람을 보냅니다.
+     */
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String[] parts = message.toString().split(":");
+        if (!parts[0].equals("fcmNotification")) return;
+        List<FcmToken> fcmTokens = fcmNotificationService.findFCMTokens(Long.parseLong(parts[1]));
+        System.out.println(fcmTokens);
+        //            fcmNotificationService.sendNotificationByToken(FcmNotificationRequestDto.of(fcmTokens, "", ""));
+        System.out.println("########## onMessage pattern " + new String(pattern) + " | " + message.toString());
+    }
+}
