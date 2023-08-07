82d2dd3bcaf03caf788b5fef7dcc5b8403bc1c38
sunwoong <sunwoong1122@gmail.com>
2023-08-08 00:30:00 +0900

    [fix] #18 FCMToken 테이블명 수정 & @Builder 중복 로직 제거

--- src/main/java/org/baggle/domain/fcm/domain/FCMToken.java
+++ src/main/java/org/baggle/domain/fcm/domain/FCMToken.java
@@ -5,31 +5,24 @@ import lombok.*;
 import org.baggle.domain.user.domain.User;
 import org.baggle.global.common.BaseTimeEntity;
 
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Getter
+@Table(name = "fcm")
 @Entity
 public class FCMToken extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "fcm_id")
     private Long id;
-
     @Column(nullable = false)
     private String fcmToken;
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Builder
-    public FCMToken(String fcmToken, User user) {
-        this.fcmToken = fcmToken;
-        this.user = user;
-    }
-
-    public void updateFcmToken(String fcmToken){
+    public void updateFcmToken(String fcmToken) {
         this.fcmToken = fcmToken;
     }
 }
