a30ba37b3f1f1a690306987484153c272edfa40c
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-08 22:56:31 +0900

    [chore] #16 modify check constraint & user schema

--- src/main/resources/db/migration/V3__modify_check_constraint.sql
+++ src/main/resources/db/migration/V3__modify_check_constraint.sql
@@ -1,3 +1,4 @@
+ALTER TABLE users DROP COLUMN nickname;
 ALTER TABLE meeting DROP COLUMN meeting_status;
 ALTER TABLE participation
     DROP COLUMN button_authority,
@@ -11,3 +12,9 @@ ALTER TABLE participation
     ADD COLUMN button_authority VARCHAR(255) CHECK (button_authority IN ('OWNER', 'NON_OWNER')),
     ADD COLUMN meeting_authority VARCHAR(255) CHECK (meeting_authority IN ('HOST', 'PARTICIPATION')),
     ADD COLUMN participation_meeting_status VARCHAR(255) CHECK (participation_meeting_status IN ('PARTICIPATING', 'DELETE'));
+
+ALTER TABLE users
+    ADD COLUMN nickname VARCHAR(255),
+    ADD COLUMN platform_id VARCHAR(255),
+    ADD COLUMN refresh_token VARCHAR(255),
+    ADD COLUMN platform VARCHAR(255) CHECK (platform IN ('KAKAO', 'APPLE', 'WITHDRAW'));
