fb09a8d3f17fce2d0afec3802657c290b75ad9bf
sunwoong <sunwoong1122@gmail.com>
2023-08-09 16:27:25 +0900

    [refactor] #21 s3 upload 메서드 user id 파라미터 제거 & validate 기능 로직 분리

--- src/main/java/org/baggle/infra/s3/S3Service.java
+++ src/main/java/org/baggle/infra/s3/S3Service.java
@@ -5,6 +5,8 @@ import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.baggle.global.error.exception.ErrorCode;
+import org.baggle.global.error.exception.InvalidValueException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,8 +16,8 @@ import java.io.InputStream;
 import java.util.UUID;
 
 @Slf4j
-@Service
 @RequiredArgsConstructor
+@Service
 public class S3Service {
     // S3Config에 등록된 S3 접근 권한 객체입니다.
     private final AmazonS3Client amazonS3Client;
@@ -24,7 +26,7 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    // yml에 등록함 base url입니다.
+    // yml에 등록된 base url입니다.
     @Value("${cloud.aws.s3.uploadPath}")
     private String defaultUrl;
 
@@ -33,20 +35,23 @@ public class S3Service {
      * param: 이미지 파일, user idx, 이미지 타입(ex. profile or post ...)
      * return: S3에 저장된 파일 이름 (db 저장 용도)
      */
-    public String uploadFile(MultipartFile multipartFile, Long userId, String imageType) {
-        if (multipartFile == null) return null;
-        if (multipartFile.isEmpty()) return null;
-        String savedFileName = getSavedFileName(multipartFile, userId, imageType);
+    public String uploadFile(MultipartFile multipartFile, String imageType) {
+        validateMultipartFile(multipartFile);
+        String savedFileName = getSavedFileName(multipartFile, imageType);
         ObjectMetadata metadata = new ObjectMetadata();
-
         try (InputStream inputStream = multipartFile.getInputStream()) {
             amazonS3Client.putObject(bucketName, savedFileName, inputStream, metadata);
         } catch (IOException e) {
             log.error("Failed to upload image", e);
-            // 추후 변경
-            // ex) throw new BusinessException(ErrorMessage.INVALID_FILE_UPLOAD);
+            throw new InvalidValueException(ErrorCode.INVALID_FILE_UPLOAD);
         }
         return getResourceUrl(savedFileName);
+    }
+
+    private void validateMultipartFile(MultipartFile multipartFile) {
+        if (multipartFile == null || multipartFile.isEmpty()) {
+            throw new InvalidValueException(ErrorCode.INVALID_MULTIPART_FILE);
+        }
     }
 
     /**
@@ -64,9 +69,9 @@ public class S3Service {
      * param: MultipartFile, user idx, 이미지 타입
      * return: 생성된 파일 이름 <--> user{번호}/{이미지 타입}/{UUID}-{입력된 파일 이름}
      */
-    private String getSavedFileName(MultipartFile multipartFile, Long userId, String imageType) {
-        return String.format("user%s/%s/%s-%s",
-                userId, imageType, getRandomUUID(), multipartFile.getOriginalFilename());
+    private String getSavedFileName(MultipartFile multipartFile, String imageType) {
+        return String.format("%s/%s-%s",
+                imageType, getRandomUUID(), multipartFile.getOriginalFilename());
     }
 
     /**
@@ -79,7 +84,7 @@ public class S3Service {
     }
 
     /**
-     * S3에 저장된 파일에 접근할 수 있는 url을 만듭니다.
+     * S3에 저장된 파일에 접근할 수 있는 URL을 만듭니다.
      * param: 저장된 파일 이름 (db에서 확인 가능)
      * return: 파일 접근 url
      */
