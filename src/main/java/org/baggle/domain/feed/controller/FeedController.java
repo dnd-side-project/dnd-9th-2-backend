3f85e4ca290a97261ab2c2e604c78f4b6d32edfd
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-09 17:27:13 +0900

    [feet] #23 긴급소집 알람 & 사진 인증 api 제작

---
+++ src/main/java/org/baggle/domain/feed/controller/FeedController.java
@@ -0,0 +1,31 @@
+package org.baggle.domain.feed.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.baggle.domain.feed.service.FeedService;
+import org.baggle.domain.user.service.AuthService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/feed")
+@Controller
+public class FeedController {
+    private final AuthService authService;
+    private final FeedService feedService;
+
+//    @PostMapping(value = "", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+//    public ResponseEntity<BaseResponse<?>> createFeedUpload(-----------------------,
+//                                                            @RequestBody FeedUploadRequestDto requestDto,
+//                                                            @RequestPart MultipartFile feedImage){
+//        FeedUploadResponseDto responseDto = feedService.createFeedUpload(user, requestDto, feedImage);
+//        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
+//    }
+
+//    @GetMapping("/{member_id}")
+//    public ResponseEntity<BaseResponse<?>> uploadNotification(________________,
+//                                                              @PathVariable("member_id") Long memberId){
+//
+//        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
+//    }
+
+}
