eab12c20a256916026eaa85136da827c856e44b6
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-11 18:09:10 +0900

    [feat] #38 모임 상세 조회 api controller 구현

--- src/main/java/org/baggle/domain/meeting/controller/MeetingController.java
+++ src/main/java/org/baggle/domain/meeting/controller/MeetingController.java
@@ -1,10 +1,17 @@
 package org.baggle.domain.meeting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.baggle.domain.meeting.dto.reponse.MeetingDetailResponseDto;
 import org.baggle.domain.meeting.service.MeetingService;
 import org.baggle.domain.user.service.AuthService;
+import org.baggle.global.common.BaseResponse;
+import org.baggle.global.common.SuccessCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/meeting")
@@ -13,10 +20,10 @@ public class MeetingController {
     private final AuthService authService;
     private final MeetingService meetingService;
 
-//    public ResponseEntity<BaseResponse<?>> findMeetingDetail(@RequestHeader("Authorization") final ________,
-//                                                             @PathVariable("meeting_id") final Long requestId){
+//    @GetMapping("/detail")
+//    public ResponseEntity<BaseResponse<?>> findMeetingDetail(@RequestParam final Long meetingId){
 //
-//        MeetingDetailResponseDto responseDto = meetingService.findMeetingDetail(requestId);
+//        MeetingDetailResponseDto responseDto = meetingService.findMeetingDetail(meetingId);
 //        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
 //    }
 
