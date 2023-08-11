c77e6614c3c8dfd0baeb30c64576fb31f6485240
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-11 18:08:28 +0900

    [feat] #38 모임 참여 & 참여 가능 여부 api controller 구현

--- src/main/java/org/baggle/domain/meeting/controller/ParticipationController.java
+++ src/main/java/org/baggle/domain/meeting/controller/ParticipationController.java
@@ -1,10 +1,18 @@
 package org.baggle.domain.meeting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.baggle.domain.meeting.dto.reponse.ParticipationAvailabilityResponseDto;
+import org.baggle.domain.meeting.dto.reponse.ParticipationResponseDto;
+import org.baggle.domain.meeting.dto.request.ParticipationReqeustDto;
 import org.baggle.domain.meeting.service.ParticipationService;
 import org.baggle.domain.user.service.AuthService;
+import org.baggle.global.common.BaseResponse;
+import org.baggle.global.common.SuccessCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/member")
@@ -13,19 +21,18 @@ public class ParticipationController {
     private final AuthService authService;
     private final ParticipationService participationService;
 
-/*    @GetMapping("/{meeting_id}")
-    public ResponseEntity<BaseResponse<?>> findMeetingAvailability(@RequestHeader("Authorization") final ________,
-                                                                                                      @PathVariable("meeting_id") final Long requestId) {
-        ParticipationAvailabilityResponseDto responseDto = participationService.findParticipationAvailability(requestId);
-        if (Objects.isNull(responseDto))
-            return ResponseEntity.ok(BaseResponse.of(SuccessCode.CREATED, null));
-        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
-    }
-
-    @PostMapping("")
-    public ResponseEntity<BaseResponse<?>> createParticipation(@RequestHeader("Authorization") final ________,
-                                                                                      @RequestBody final ParticipationReqeustDto requestDto) {
-        ParticipationResponseDto responseDto = participationService.createParticipation(, requestDto);
-        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
-    }*/
+//    @GetMapping()
+//    public ResponseEntity<BaseResponse<?>> findMeetingAvailability(@RequestParam final Long meetingId) {
+//        final ParticipationAvailabilityResponseDto responseDto = participationService.findParticipationAvailability(, meetingId);
+//        if (Objects.isNull(responseDto))
+//            return ResponseEntity.ok(BaseResponse.of(SuccessCode.CREATED, null));
+//        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
+//    }
+//
+//    @PostMapping()
+//    public ResponseEntity<BaseResponse<?>> createParticipation(@RequestBody final ParticipationReqeustDto requestDto) {
+//        System.out.println(requestDto);
+//        final ParticipationResponseDto responseDto = participationService.createParticipation(, requestDto);
+//        return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
+//    }
 }
