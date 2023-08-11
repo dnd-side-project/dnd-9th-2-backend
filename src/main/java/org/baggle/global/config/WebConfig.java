8aedd931463892849ff4ec649e5ee94247b151fd
sunwoong <sunwoong1122@gmail.com>
2023-08-12 03:25:08 +0900

    [chore] #42 argument resolver 등록 설정 추가

---
+++ src/main/java/org/baggle/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.baggle.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.baggle.global.config.auth.UserIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final UserIdArgumentResolver userIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
+    }
+}
