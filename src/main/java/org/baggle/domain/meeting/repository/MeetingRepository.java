c1ba0f64e0612620a2e7cc3178a92ece9421a3aa
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-13 02:31:49 +0900

    [feat] #50 시간 범위내 모임을 조회하는 쿼리 작성

--- src/main/java/org/baggle/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/org/baggle/domain/meeting/repository/MeetingRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
@@ -13,4 +14,10 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     @Query("SELECT m FROM Meeting m WHERE " +
             "m.date = :currentDate AND m.time between :prevTime and :currentTime ")
     List<Meeting> findMeetingsStartingSoon(@Param(value = "prevTime") LocalTime prevTime, @Param(value = "currentDate") LocalDate currentDate, @Param(value = "currentTime") LocalTime currentTime);
+
+    @Query("SELECT m FROM Meeting m " +
+            "JOIN Participation p ON m = p.meeting " +
+            "WHERE STR_TO_DATE(CONCAT(m.date, ' ', m.time), '%Y-%m-%d %H:%i:%s') BETWEEN :from AND :to " +
+            "AND p.user.id = :userId")
+    List<Meeting> findMeetingsWithinTimeRange(@Param("userId") Long userId, @Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
 }
