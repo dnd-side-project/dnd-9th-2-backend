b16dde3a2af91c70c335f20006bcc809c27a6cf9
sunwoong <sunwoong1122@gmail.com>
2023-08-12 15:34:29 +0900

    [feat] #44 로그인 비즈니스 로직 구현 & 기존 코드 리팩터링

--- src/main/java/org/baggle/domain/user/service/AuthService.java
+++ src/main/java/org/baggle/domain/user/service/AuthService.java
@@ -1,6 +1,8 @@
 package org.baggle.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.baggle.domain.fcm.domain.FcmToken;
+import org.baggle.domain.fcm.repository.FcmRepository;
 import org.baggle.domain.user.auth.apple.AppleOAuthProvider;
 import org.baggle.domain.user.domain.Platform;
 import org.baggle.domain.user.domain.RefreshToken;
@@ -28,33 +30,29 @@ import java.util.List;
 public class AuthService {
     private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final FcmRepository fcmRepository;
     private final S3Service s3Service;
     private final JwtProvider jwtProvider;
     private final AppleOAuthProvider appleOAuthProvider;
 
-    // TODO
     public UserAuthResponseDto signIn(String token, UserSignInRequestDto userSignInRequestDto) {
-        return null;
+        User findUser = getUser(token, userSignInRequestDto);
+        updateFcmToken(userSignInRequestDto.getFcmToken(), findUser);
+        Token issuedToken = issueAccessTokenAndRefreshToken(findUser);
+        return UserAuthResponseDto.of(issuedToken, findUser);
     }
 
     public UserAuthResponseDto signUp(String token, MultipartFile image, String nickname, String platform, String fcmToken) {
-        validateDuplicateNickname(nickname);
-        Platform enumPlatform = getEnumPlatformFromStringPlatform(platform);
-        String platformId = getPlatformIdFromToken(token, enumPlatform);
-        validateDuplicateUser(enumPlatform, platformId);
-        String profileImageUrl = hasMultipartFile(image) ? uploadProfileImageToS3AndGetProfileImageUrl(image, ImageType.PROFILE) : "";
-        User user = User.createUserWithFcmToken(profileImageUrl, nickname, fcmToken, platformId, enumPlatform);
-        User savedUser = userRepository.save(user);
-        Token issuedToken = jwtProvider.issueToken(savedUser.getId());
+        User savedUser = validateUserAndSaveUser(token, image, nickname, platform, fcmToken);
+        Token issuedToken = issueAccessTokenAndRefreshToken(savedUser);
         updateRefreshToken(savedUser, issuedToken.getRefreshToken());
         return UserAuthResponseDto.of(issuedToken, savedUser);
     }
 
     public void withdraw(Long userId) {
-        User findUser = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
+        User findUser = getUser(userId);
         findUser.withdrawUser();
-        refreshTokenRepository.deleteById(userId);
+        deleteRefreshToken(userId);
     }
 
     // TODO
@@ -62,11 +60,42 @@ public class AuthService {
         return null;
     }
 
-    private void validateDuplicateNickname(String nickname) {
-        List<User> findUsers = userRepository.findUsersByNickname(nickname);
-        if (!findUsers.isEmpty()) {
-            throw new ConflictException(ErrorCode.DUPLICATE_NICKNAME);
-        }
+    private User getUser(String token, UserSignInRequestDto userSignInRequestDto) {
+        Platform enumPlatform = getEnumPlatformFromStringPlatform(userSignInRequestDto.getPlatform());
+        String platformId = getPlatformIdFromToken(token, enumPlatform);
+        return getUserByPlatformAndPlatformId(enumPlatform, platformId);
+    }
+
+    private User getUser(Long userId) {
+        return getUserById(userId);
+    }
+
+    private void deleteRefreshToken(Long userId) {
+        refreshTokenRepository.deleteById(userId);
+    }
+
+    private void updateFcmToken(String fcmToken, User user) {
+        FcmToken findFcmToken = getFcmTokenByUserId(user.getId());
+        findFcmToken.updateFcmToken(fcmToken);
+    }
+
+    private User validateUserAndSaveUser(String token, MultipartFile image, String nickname, String platform, String fcmToken) {
+        validateDuplicateNickname(nickname);
+        Platform enumPlatform = getEnumPlatformFromStringPlatform(platform);
+        String platformId = getPlatformIdFromToken(token, enumPlatform);
+        validateDuplicateUser(enumPlatform, platformId);
+        String profileImageUrl = hasMultipartFile(image) ? uploadProfileImageToS3AndGetProfileImageUrl(image, ImageType.PROFILE) : "";
+        User user = User.createUserWithFcmToken(profileImageUrl, nickname, fcmToken, platformId, enumPlatform);
+        return userRepository.save(user);
+    }
+
+    private Token issueAccessTokenAndRefreshToken(User user) {
+        return jwtProvider.issueToken(user.getId());
+    }
+
+    private void updateRefreshToken(User user, String refreshToken) {
+        user.updateRefreshToken(refreshToken);
+        refreshTokenRepository.save(RefreshToken.createRefreshToken(user.getId(), refreshToken));
     }
 
     private Platform getEnumPlatformFromStringPlatform(String platform) {
@@ -81,6 +110,28 @@ public class AuthService {
         return appleOAuthProvider.getApplePlatformId(token);
     }
 
+    private User getUserByPlatformAndPlatformId(Platform platform, String platformId) {
+        return userRepository.findUserByPlatformAndPlatformId(platform, platformId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private FcmToken getFcmTokenByUserId(Long userId) {
+        return fcmRepository.findByUserId(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.FCM_TOKEN_NOT_FOUND));
+    }
+
+    private void validateDuplicateNickname(String nickname) {
+        List<User> findUsers = userRepository.findUsersByNickname(nickname);
+        if (!findUsers.isEmpty()) {
+            throw new ConflictException(ErrorCode.DUPLICATE_NICKNAME);
+        }
+    }
+
     private void validateDuplicateUser(Platform platform, String platformId) {
         List<User> findUsers = userRepository.findUsersByPlatformAndPlatformId(platform, platformId);
         if (!findUsers.isEmpty()) {
@@ -90,11 +141,6 @@ public class AuthService {
 
     private boolean hasMultipartFile(MultipartFile multipartFile) {
         return multipartFile != null && !multipartFile.isEmpty();
-    }
-
-    private void updateRefreshToken(User user, String refreshToken) {
-        user.updateRefreshToken(refreshToken);
-        refreshTokenRepository.save(RefreshToken.createRefreshToken(user.getId(), refreshToken));
     }
 
     private String uploadProfileImageToS3AndGetProfileImageUrl(MultipartFile image, ImageType imageType) {
