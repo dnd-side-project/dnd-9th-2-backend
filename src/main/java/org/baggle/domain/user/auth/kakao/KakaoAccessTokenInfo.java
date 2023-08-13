f0eb5b99c19606c529d6b75cd58698aa2635d0f6
sunwoong <sunwoong1122@gmail.com>
2023-08-13 18:10:52 +0900

    [feat] #52 카카오 oauth access token info 응답 dto 구현

---
+++ src/main/java/org/baggle/domain/user/auth/kakao/KakaoAccessTokenInfo.java
@@ -0,0 +1,11 @@
+package org.baggle.domain.user.auth.kakao;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class KakaoAccessTokenInfo {
+    private Long id;
+}
