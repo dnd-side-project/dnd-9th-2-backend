7b2eb2b80d5f70da9c190e807326e05036a06c18
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-14 02:23:41 +0900

    [feat] #55 redis에 등록된 데이터를 구별할 수 있는 enum class 제작

---
+++ src/main/java/org/baggle/infra/redis/RedisDataType.java
@@ -0,0 +1,28 @@
+package org.baggle.infra.redis;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.baggle.domain.user.domain.Platform;
+import org.baggle.global.error.exception.ErrorCode;
+import org.baggle.global.error.exception.InvalidValueException;
+
+import java.util.Arrays;
+
+import static org.baggle.global.error.exception.ErrorCode.INVALID_REDIS_DATA_TYPE;
+
+@RequiredArgsConstructor
+@Getter
+public enum RedisDataType {
+    REFRESHTOKEN("refreshToken"),
+    FCM_NOTIFICATION("fcmNoticiation"),
+    FCM_TIMER("fcmTimer");
+
+    private final String stringPlatform;
+
+    public static RedisDataType getEnumRedisDataTypeFromString(String stringRedisType){
+        return Arrays.stream(values())
+                .filter(redisDataType -> redisDataType.stringPlatform.equals(stringRedisType))
+                .findFirst()
+                .orElseThrow(()-> new InvalidValueException(INVALID_REDIS_DATA_TYPE));
+    }
+}
