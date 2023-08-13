7ba0bc5bfcc840cd9c28952c3e065933b161a7bc
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-13 14:58:42 +0900

    [refactor] #50 import 코드 정렬

--- src/main/java/org/baggle/domain/meeting/dto/request/UpdateMeetingInfoRequestDto.java
+++ src/main/java/org/baggle/domain/meeting/dto/request/UpdateMeetingInfoRequestDto.java
@@ -3,9 +3,9 @@ package org.baggle.domain.meeting.dto.request;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class UpdateMeetingInfoRequestDto {
