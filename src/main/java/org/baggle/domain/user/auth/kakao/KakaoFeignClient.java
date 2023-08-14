f6a322a4c8da71b04f7730bf3d1806973c7117ca
sunwoong <sunwoong1122@gmail.com>
2023-08-14 22:33:38 +0900

    [fix] #66 카카오 oauth feign client name 변경

--- src/main/java/org/baggle/domain/user/auth/kakao/KakaoFeignClient.java
+++ src/main/java/org/baggle/domain/user/auth/kakao/KakaoFeignClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "kakao-access-token-info-feign-client", url = "https://kapi.kakao.com/v1/user/access_token_info")
-public interface KakaoAccessTokenInfoFeignClient {
+@FeignClient(name = "kakao-feign-client", url = "https://kapi.kakao.com/v1/user/access_token_info")
+public interface KakaoFeignClient {
     @GetMapping
     KakaoAccessTokenInfo getKakaoAccessTokenInfo(@RequestHeader("Authorization") String accessToken);
 }
