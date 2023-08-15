6b32d7fec75d7476c02ca071b41f1f9027a980ee
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-16 00:09:34 +0900

    [feat] #78 서버 시간 한국 기준으로 맞추기

--- src/main/java/org/baggle/BaggleApplication.java
+++ src/main/java/org/baggle/BaggleApplication.java
@@ -1,8 +1,11 @@
 package org.baggle;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class BaggleApplication {
     // aws sdk 연동
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
     public static void main(String[] args) {
