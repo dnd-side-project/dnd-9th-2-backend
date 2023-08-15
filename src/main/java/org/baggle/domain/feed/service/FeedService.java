889891b7fac4ea54afbad03bbeabe61ad119dfe6
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-16 00:02:25 +0900

    [refactor] #78 오류 처리 추상화

--- src/main/java/org/baggle/domain/feed/service/FeedService.java
+++ src/main/java/org/baggle/domain/feed/service/FeedService.java
@@ -1,7 +1,6 @@
 package org.baggle.domain.feed.service;
 
 import lombok.RequiredArgsConstructor;
-import org.baggle.domain.fcm.domain.FcmTimer;
 import org.baggle.domain.fcm.domain.FcmToken;
 import org.baggle.domain.fcm.dto.request.FcmNotificationRequestDto;
 import org.baggle.domain.fcm.repository.FcmRepository;
@@ -28,7 +27,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 import static org.baggle.global.error.exception.ErrorCode.*;
 
@@ -50,13 +48,12 @@ public class FeedService {
      * throw 긴급소집 이벤트가 진행 중이 아닌 경우
      */
     public FeedUploadResponseDto feedUpload(FeedUploadRequestDto requestDto, MultipartFile feedImage) {
-        Participation participation = participationRepository.findById(requestDto.getParticipationId()).orElseThrow(() -> new EntityNotFoundException(PARTICIPATION_NOT_FOUND));
-        if (!validateCertificationTime(participation.getMeeting(), requestDto.getAuthorizationTime()))
-            throw new InvalidValueException(INVALID_CERTIFICATION_TIME);
+        Participation participation = getParticipation(requestDto.getMemberId());
+        validateCertificationTime(participation.getMeeting());
         String imgUrl = s3Provider.uploadFile(feedImage, ImageType.FEED.getImageType());
         Feed feed = Feed.createParticipationWithFeedImg(participation, imgUrl);
         feedRepository.save(feed);
-        return FeedUploadResponseDto.of(feed.getId());
+        return FeedUploadResponseDto.of(feed.getId(), feed.getFeedImageUrl());
     }
 
     /**
@@ -65,37 +62,40 @@ public class FeedService {
      * throw 버튼 활성화 가능 시간이 아닌 경우
      */
     public FeedNotificationResponseDto uploadNotification(Long requestId, LocalDateTime authorizationTime) {
-        Participation participation = participationRepository.findById(requestId).orElseThrow(() -> new EntityNotFoundException(PARTICIPATION_NOT_FOUND));
-        if (participation.getMeeting().getMeetingStatus() != MeetingStatus.SCHEDULED)
-            throw new InvalidValueException(INVALID_MEETING_TIME);
-        if (!validateNotificationTime(participation.getMeeting(), authorizationTime))
-            throw new InvalidValueException(INVALID_CERTIFICATION_TIME);
+        Participation participation = getParticipation(requestId);
+        validateMeetingStatusForScheduled(participation.getMeeting());
+        validateNotificationTime(participation.getMeeting(), authorizationTime);
 //        broadcastNotification(participation.getMeeting());
         // 이벤트 로직 5분 타이머를 시작하는 code 입니다.
         startEmergencyNotificationEvent(participation, authorizationTime);
         return FeedNotificationResponseDto.of(participation.getMeeting(), authorizationTime);
     }
 
-    private Meeting getMeeting(Long meetingId){
-        return meetingRepository.findById(meetingId).orElseThrow(() -> new EntityNotFoundException(MEETING_NOT_FOUND));
+    private Meeting getMeeting(Long meetingId) {
+        return meetingRepository.findById(meetingId)
+                .orElseThrow(() -> new EntityNotFoundException(MEETING_NOT_FOUND));
     }
 
-    /**
-     * 긴급 소집 이벤트 활성화 여부를 확인하는 코드
-     * return: 긴급 소집 이벤트 활성화시 True else False
-     */
-    private Boolean validateCertificationTime(Meeting meeting, LocalDateTime authorizationTime) {
-        FcmTimer fcmTimer = fcmTimerRepository.findById(meeting.getId()).orElse(null);
-        if (Objects.isNull(fcmTimer)) return Boolean.FALSE;
-        Duration duration = Duration.between(fcmTimer.getStartTime(), authorizationTime);
-        return 0 <= duration.toSeconds() && duration.toSeconds() <= 300;
+    private Participation getParticipation(Long participationId) {
+        return participationRepository.findById(participationId)
+                .orElseThrow(() -> new EntityNotFoundException(PARTICIPATION_NOT_FOUND));
     }
 
-    private Boolean validateNotificationTime(Meeting meeting, LocalDateTime authorizationTime) {
+    private void validateMeetingStatusForScheduled(Meeting meeting) {
+        if (meeting.getMeetingStatus() != MeetingStatus.SCHEDULED)
+            throw new InvalidValueException(INVALID_MEETING_TIME);
+    }
+
+    private void validateCertificationTime(Meeting meeting) {
+        if (meeting.getMeetingStatus() != MeetingStatus.ONGOING)
+            throw new InvalidValueException(INVALID_CERTIFICATION_TIME);
+    }
+
+    private void validateNotificationTime(Meeting meeting, LocalDateTime authorizationTime) {
         LocalDateTime meetingTime = LocalDateTime.of(meeting.getDate(), meeting.getTime());
         Duration duration = Duration.between(authorizationTime, meetingTime);
-        return 0 <= duration.toSeconds() && duration.toSeconds() <= 1800;
-
+        if (!(0 <= duration.toSeconds() && duration.toSeconds() <= 1800))
+            throw new InvalidValueException(INVALID_CERTIFICATION_TIME);
     }
 
     private void broadcastNotification(Meeting meeting) {
@@ -104,7 +104,7 @@ public class FeedService {
         fcmNotificationService.sendNotificationByToken(fcmNotificationRequestDto);
     }
 
-    private void startEmergencyNotificationEvent(Participation participation, LocalDateTime authorizationTime){
+    private void startEmergencyNotificationEvent(Participation participation, LocalDateTime authorizationTime) {
         fcmNotificationService.deleteFcmNotification(participation.getMeeting().getId());
         fcmNotificationService.createFcmTimer(participation.getMeeting().getId(), authorizationTime);
         Meeting meeting = getMeeting(participation.getMeeting().getId());
