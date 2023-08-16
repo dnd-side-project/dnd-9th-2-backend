84a04789b67acb5dba2807c01f5b76d4df62e9c3
sunwoong <sunwoong1122@gmail.com>
2023-08-16 18:08:13 +0900

    [feat] #77 모임 생성 API 구현

--- src/main/java/org/baggle/domain/meeting/controller/MeetingApiController.java
+++ src/main/java/org/baggle/domain/meeting/controller/MeetingApiController.java
@@ -1,10 +1,12 @@
 package org.baggle.domain.meeting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.baggle.domain.meeting.dto.request.CreateMeetingRequestDto;
+import org.baggle.domain.meeting.dto.request.UpdateMeetingInfoRequestDto;
 import org.baggle.domain.meeting.dto.response.MeetingDetailResponseDto;
 import org.baggle.domain.meeting.dto.response.UpdateMeetingInfoResponseDto;
-import org.baggle.domain.meeting.dto.request.UpdateMeetingInfoRequestDto;
 import org.baggle.domain.meeting.service.MeetingDetailService;
+import org.baggle.domain.meeting.service.MeetingService;
 import org.baggle.global.common.BaseResponse;
 import org.baggle.global.common.SuccessCode;
 import org.baggle.global.config.auth.UserId;
@@ -16,7 +18,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/meeting")
 @Controller
 public class MeetingApiController {
+    private final MeetingService meetingService;
     private final MeetingDetailService meetingDetailService;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse<?>> createMeeting(@UserId final Long userId,
+                                                         @RequestBody final CreateMeetingRequestDto createMeetingRequestDto) {
+        meetingService.createMeeting(userId, createMeetingRequestDto);
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.CREATED, true));
+    }
 
     @GetMapping("/detail")
     public ResponseEntity<BaseResponse<?>> findMeetingDetail(@UserId final Long userId,
@@ -31,6 +41,4 @@ public class MeetingApiController {
         final UpdateMeetingInfoResponseDto responseDto = meetingDetailService.updateMeetingInfo(userId, requestDto);
         return ResponseEntity.ok(BaseResponse.of(SuccessCode.OK, responseDto));
     }
-
-
 }
