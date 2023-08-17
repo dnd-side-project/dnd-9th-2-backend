82cf2e5712712f87e94eaf03f08805e57be0f9ac
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-18 00:24:36 +0900

    [feat] #88 MessageSource Config 설정

---
+++ src/main/java/org/baggle/global/config/MessageConfig.java
@@ -0,0 +1,17 @@
+package org.baggle.global.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+@Configuration
+public class MessageConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+}
