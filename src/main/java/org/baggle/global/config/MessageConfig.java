4f7143314cd6316051134deba5ef12ec2377ac64
sunwoong <sunwoong1122@gmail.com>
2023-08-18 16:58:15 +0900

    [chore] #85 코드 정렬

--- src/main/java/org/baggle/global/config/MessageConfig.java
+++ src/main/java/org/baggle/global/config/MessageConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+
 @Configuration
 public class MessageConfig {
     @Bean
