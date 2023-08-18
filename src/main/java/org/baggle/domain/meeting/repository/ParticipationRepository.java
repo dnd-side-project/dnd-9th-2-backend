b119c60d24613f541035c56885b046bc8c17d8a6
sunwoong <sunwoong1122@gmail.com>
2023-08-18 16:58:04 +0900

    [fix] #85 누락된 @Param 애노테이션 추가

--- src/main/java/org/baggle/domain/meeting/repository/ParticipationRepository.java
+++ src/main/java/org/baggle/domain/meeting/repository/ParticipationRepository.java
@@ -6,17 +6,20 @@ import org.baggle.domain.meeting.domain.Meeting;
 import org.baggle.domain.meeting.domain.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
-    @Query("SELECT fcm.user.fcmToken FROM Participation p " +
+    @Query("SELECT fcm.user.fcmToken " +
+            "FROM Participation p " +
             "JOIN p.meeting m " +
             "JOIN p.user u " +
             "JOIN u.fcmToken fcm " +
-            "WHERE p.buttonAuthority = :buttonAuthority AND m = :meeting")
-    List<FcmToken> findFcmTokensByMeetingAndButtonAuthority(Meeting meeting, ButtonAuthority buttonAuthority);
+            "WHERE p.buttonAuthority = :buttonAuthority " +
+            "AND m = :meeting")
+    List<FcmToken> findFcmTokensByMeetingAndButtonAuthority(@Param("meeting") Meeting meeting, @Param("buttonAuthority") ButtonAuthority buttonAuthority);
 
     Optional<Participation> findByUserIdAndMeetingId(Long userId, Long meetingId);
 }
