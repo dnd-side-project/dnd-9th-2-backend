e27c9f0c7b8453998bd5a12f13c0d7f50daa9696
RyuKwanKon <rhks33rhs@gmail.com>
2023-08-31 22:24:29 +0900

    [fix] #135 모임 수정 api request 수정

--- src/main/java/org/baggle/domain/meeting/dto/request/UpdateMeetingInfoRequestDto.java
+++ src/main/java/org/baggle/domain/meeting/dto/request/UpdateMeetingInfoRequestDto.java
@@ -3,8 +3,8 @@ package org.baggle.domain.meeting.dto.request;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
-import java.time.LocalTime;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -12,7 +12,6 @@ public class UpdateMeetingInfoRequestDto {
     private Long meetingId;
     private String title;
     private String place;
-    private LocalDate date;
-    private LocalTime time;
+    private LocalDateTime dateTime;
     private String memo;
 }
