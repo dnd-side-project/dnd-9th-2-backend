cc71456d09bba63bc1d740f48815af2dd2961f30
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-11 02:33:13 +0900

    [del] #156 불필요 메서드 제거

--- src/main/java/org/baggle/domain/fcm/service/FcmNotificationService.java
+++ src/main/java/org/baggle/domain/fcm/service/FcmNotificationService.java
@@ -4,7 +4,6 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.baggle.domain.fcm.domain.FcmNotification;
@@ -13,15 +12,11 @@ import org.baggle.domain.fcm.domain.FcmToken;
 import org.baggle.domain.fcm.dto.request.FcmNotificationRequestDto;
 import org.baggle.domain.fcm.repository.FcmNotificationRepository;
 import org.baggle.domain.fcm.repository.FcmTimerRepository;
-import org.baggle.domain.meeting.domain.ButtonAuthority;
-import org.baggle.domain.meeting.domain.Meeting;
-import org.baggle.domain.meeting.repository.ParticipationRepository;
 import org.baggle.global.error.exception.ErrorCode;
 import org.baggle.global.error.exception.InvalidValueException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,7 +25,6 @@ public class FcmNotificationService {
     private final FirebaseMessaging firebaseMessaging;
     private final FcmNotificationRepository fcmNotificationRepository;
     private final FcmTimerRepository fcmTimerRepository;
-    private final ParticipationRepository participationRepository;
 
     public void createFcmNotification(Long key) {
         FcmNotification fcmNotification = FcmNotification.builder()
@@ -42,11 +36,6 @@ public class FcmNotificationService {
 
     public void deleteFcmNotification(Long key) {
         fcmNotificationRepository.deleteById(key);
-    }
-
-    @Transactional
-    public List<FcmToken> findFcmTokensByButtonAuthority(Meeting meeting, ButtonAuthority buttonAuthority) {
-        return participationRepository.findFcmTokensByMeetingAndButtonAuthority(meeting, buttonAuthority);
     }
 
     public void sendNotificationByToken(FcmNotificationRequestDto fcmNotificationRequestDto, Long meetingId) {
