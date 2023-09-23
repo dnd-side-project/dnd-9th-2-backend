f2e1a1aa0cc97a250d821fa6a9a7891a04a66d57
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-24 00:43:35 +0900

    [feat] #179 신고 데이터 존재 여부 확인 메서드 구현

--- src/main/java/org/baggle/domain/report/repository/ReportRepository.java
+++ src/main/java/org/baggle/domain/report/repository/ReportRepository.java
@@ -1,7 +1,9 @@
 package org.baggle.domain.report.repository;
 
 import org.baggle.domain.report.domain.Report;
+import org.baggle.domain.user.domain.Platform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    boolean existsReportByFeedIdAndParticipationId(Long feedID, Long participationId);
 }
