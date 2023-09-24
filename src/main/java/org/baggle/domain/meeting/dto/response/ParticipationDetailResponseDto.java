325e733370b496328130a0b366ce8a22b06443a7
RyuKwanKon <rhks33rhs@gmail.com>
2023-09-24 12:53:37 +0900

    [refactor] #179 별도의 @Builder 정의 했던 중복 코드 제거

--- src/main/java/org/baggle/domain/meeting/dto/response/ParticipationDetailResponseDto.java
+++ src/main/java/org/baggle/domain/meeting/dto/response/ParticipationDetailResponseDto.java
@@ -8,36 +8,29 @@ import org.baggle.domain.meeting.domain.MeetingAuthority;
 import org.baggle.domain.meeting.domain.Participation;
 import org.baggle.domain.user.domain.User;
 
-import java.util.Objects;
-
+@Builder
 @Getter
 public class ParticipationDetailResponseDto {
     private Long memberId;
     private String nickname;
     private String profileImageUrl;
-    private Boolean meetingAuthority;
-    private Boolean buttonAuthority;
+    private boolean meetingAuthority;
+    private boolean buttonAuthority;
     private Long feedId;
     private String feedImageUrl;
     private boolean report;
 
-    @Builder
-    public ParticipationDetailResponseDto(User user, Participation participation, Feed feed, boolean report) {
-        this.memberId = participation.getId();
-        this.nickname = user.getNickname();
-        this.profileImageUrl = user.getProfileImageUrl();
-        this.meetingAuthority = (participation.getMeetingAuthority() == MeetingAuthority.HOST) ? Boolean.TRUE : Boolean.FALSE;
-        this.buttonAuthority = (participation.getButtonAuthority() == ButtonAuthority.OWNER) ? Boolean.TRUE : Boolean.FALSE;
-        this.feedId = Objects.isNull(feed) ? null : feed.getId();
-        this.feedImageUrl = Objects.isNull(feed) ? "" : feed.getFeedImageUrl();
-        this.report = report;
-    }
-
     public static ParticipationDetailResponseDto of(Participation participation, boolean report) {
+        User participationUser = participation.getUser();
+        Feed participationFeed = participation.getFeed();
         return ParticipationDetailResponseDto.builder()
-                .user(participation.getUser())
-                .participation(participation)
-                .feed(participation.getFeed())
+                .memberId(participation.getId())
+                .nickname(participationUser.getNickname())
+                .profileImageUrl(participationUser.getProfileImageUrl())
+                .meetingAuthority(participation.getMeetingAuthority() == MeetingAuthority.HOST)
+                .buttonAuthority(participation.getButtonAuthority() == ButtonAuthority.OWNER)
+                .feedId(participationFeed.getId())
+                .feedImageUrl(participationFeed.getFeedImageUrl())
                 .report(report)
                 .build();
     }
