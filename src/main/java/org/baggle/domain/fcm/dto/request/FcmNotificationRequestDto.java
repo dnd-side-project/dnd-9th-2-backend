3f21a0feee138d40ace642cfbef3216d0fe82542
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-28 11:13:16 +0900

    [refactor] #188 Builder 애노테이션 접근 범위 private으로 변경

--- src/main/java/org/baggle/domain/fcm/dto/request/FcmNotificationRequestDto.java
+++ src/main/java/org/baggle/domain/fcm/dto/request/FcmNotificationRequestDto.java
@@ -1,18 +1,18 @@
 package org.baggle.domain.fcm.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import org.baggle.domain.fcm.domain.FcmToken;
 
 import java.util.List;
 
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class FcmNotificationRequestDto {
     private List<FcmToken> targetTokenList;
     private String title;
     private String body;
-
 
     public static FcmNotificationRequestDto of(List<FcmToken> targetTokenList, String title, String body) {
         return FcmNotificationRequestDto.builder()
